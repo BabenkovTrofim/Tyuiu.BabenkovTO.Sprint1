45a660679eb91747e475803c503a01852b0f402c
BabenkovTrofim <babenkovt@bk.ru>
2025-09-14 16:12:48 +0500

    Task1

---
+++ Tyuiu.BabenkovTO.Sprint1.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BabenkovTO.Sprint1.Task1.V28.Lib;
+namespace Tyuiu.BabenkovTO.Sprint1.Task1.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate()
+        {
+            DataService ds = new DataService();
+            double i = 4.0;
+            var res = ds.Calculate(i);
+            Assert.AreEqual(1.6, res);
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint1.Task1.V28/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BabenkovTO.Sprint1.Task1.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #1 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Организация ввода-вывода в консольных приложениях                 *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (x*2)/(1+x) и печатает его на экране.    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double x;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        Console.WriteLine(ds.Calculate(x));
+    }
+}
