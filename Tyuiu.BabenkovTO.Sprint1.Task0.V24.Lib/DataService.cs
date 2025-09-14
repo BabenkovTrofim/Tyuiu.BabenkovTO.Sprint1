797e7b5d8d9045170f3ed48e0cd57024276124f8
BabenkovTrofim <babenkovt@bk.ru>
2025-09-14 14:42:52 +0500

    Add project files.

---
+++ Tyuiu.BabenkovTO.Sprint1.Task0.V24.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.BabenkovTO.Sprint1.Task0.V24.Lib
+{
+    public class DataService
+    {
+        public static 
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint1.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BabenkovTO.Sprint1.Task0.V24.Lib;
+namespace Tyuiu.BabenkovTO.Sprint1.Task0.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(2, ds.Calculate);
+        }
+    }
+}
