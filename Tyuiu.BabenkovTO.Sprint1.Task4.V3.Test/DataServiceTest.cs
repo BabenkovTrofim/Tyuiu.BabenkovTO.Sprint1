b9a4e36dec21c842d65d62f995dfa3e47291178f
BabenkovTrofim <babenkovt@bk.ru>
2025-09-18 12:33:28 +0500

    Task4

---
+++ Tyuiu.BabenkovTO.Sprint1.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BabenkovTO.Sprint1.Task4.V3.Lib;
+namespace Tyuiu.BabenkovTO.Sprint1.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalc()
+        {
+            double x = 27;
+            double y = 16;
+            DataService ds = new DataService();
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.074, res);
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint1.Task4.V3/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BabenkovTO.Sprint1.Task4.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #1 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Класс Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double x, y;
+        Console.Write("x = ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("y = ");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        var res = ds.Calculate(x, y);
+        Console.WriteLine($"√(3 * x) / (x - y)^2 = {res}");
+    }
+}
